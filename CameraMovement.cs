664b04df5948bf9258f9ae3732e9a512ef34105f
Zach Battal <60168662+Ridde999@users.noreply.github.com>
2020-03-23 01:27:34 +0300

    Basic game script version 1.0
    Camera movement and player/car movement

---
+++ CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform car;
+    public float upValue;
+    
+    private Vector3 offset;
+    
+    void Start()
+    {
+        offset = new Vector3(0, upValue, 0);
+    }
+
+    void LateUpdate()
+    {
+        transform.position = car.position + offset;
+    }
+}
---
+++ Movement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public float turnSpeed;
+
+    void Update()
+    {
+        transform.Translate(-1 * speed *Time.deltaTime, 0, 0);
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+        }
+    }
+}
